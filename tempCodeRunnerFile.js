608e1d1c4601c4734a606123afcf123b11adda64
Nidhi Chaudhary <106021001+nidhichaudhary2002@users.noreply.github.com>
2024-02-13 19:44:51 +0530

    remade a new repo

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+import authRoutes from "./routes/authRoute.js"
+
+//configuration env
+// dotenv.config();
+
+// //database config
+// connectDB();
+
+// //rest object
+// const app =express();
+
+// //middleware
+// app.use(express.json())
+// app.use(morgan('dev'))
+
+
+// //routes
